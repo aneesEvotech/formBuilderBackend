6d990eea103a6afb02f0d4b76d2014c4dc16a898
Anees Khan <aneesleaders@gmail.com>
2025-05-10 10:29:36 +0530

    chnage in core

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ const authroute = require("./authapiroute");
 const responseroute = require("./responseroute");
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://formbuilderevotechfrontend.vercel.app",
+];
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
-app.use(
-  cors({
-    origin: "https://formbuilderevotechfrontend.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
