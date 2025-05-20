901c7587c17e797465d0513e02abe7df0fea044a
aneesEvotech <anees@evotech.global>
2025-05-20 23:26:00 +0530

    changes in bacj

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://formbuilderevotechfrontend.vercel.app",
+  // "https://formbuilderevotechfrontend.vercel.app",
   "https://form-builder-frontends.vercel.app"
 ];
 
